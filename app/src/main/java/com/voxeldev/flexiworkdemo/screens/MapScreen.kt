06392e3c2e7c790bb9bf86fd7a6b6a5c3e34bc5b
nVoxel <nvoxel@outlook.com>
2024-05-25 13:59:37 +0300

    Fix map screen

--- app/src/main/java/com/voxeldev/flexiworkdemo/screens/MapScreen.kt
+++ app/src/main/java/com/voxeldev/flexiworkdemo/screens/MapScreen.kt
@@ -3,13 +3,15 @@ package com.voxeldev.flexiworkdemo.screens
 import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -18,24 +20,25 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.voxeldev.flexiworkdemo.R
 import com.voxeldev.flexiworkdemo.components.PreviewBase
+import com.voxeldev.flexiworkdemo.navigation.LocalNavController
+import com.voxeldev.flexiworkdemo.navigation.NavigationScreen
 
 /**
  * @author nvoxel
  */
 @Composable
 fun MapScreen() {
+    val navController = LocalNavController.current
 
     var titleSearch by rememberSaveable { mutableStateOf("") }
 
@@ -55,30 +58,23 @@ fun MapScreen() {
             value = titleSearch,
             onValueChange = { titleSearch = it },
             placeholder = {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text(
-                        modifier = Modifier
-                            .weight(1f),
-                        textAlign = TextAlign.Start,
-                        overflow = TextOverflow.Ellipsis,
-                        text = "Введите пункт назначения"
-                    )
-                    Image(
-                        modifier = Modifier
-                            .size(width = 25.dp, height = 25.dp),
-                        painter = painterResource(id = R.drawable.ic_search),
-                        contentDescription = "Введите пункт назначения"
-                    )
+                Text(
+                    overflow = TextOverflow.Ellipsis,
+                    text = "Введите пункт назначения",
+                )
+            },
+            trailingIcon = {
+                if (titleSearch.isNotEmpty()) {
+                    IconButton(onClick = { navController.navigate(NavigationScreen.List.routeWithArguments) }) {
+                        Icon(imageVector = Icons.Default.Search, contentDescription = "Поиск")
+                    }
                 }
             },
             shape = RoundedCornerShape(size = 32.dp)
         )
 
-        ListButton (
-            onClick = {}
+        ListButton(
+            onClick = { navController.navigate(NavigationScreen.List.routeWithArguments) }
         )
 
         Image(
